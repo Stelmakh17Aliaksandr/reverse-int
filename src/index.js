8801f68c2c79efbf5d5adeb628a4c94df1e1451c
Stelmakh17Aliaksandr <70840846+Stelmakh17Aliaksandr@users.noreply.github.com>
2020-10-22 14:49:30 +0300

    feat: upgrade index.html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  n = String(n)
+    let result = ""
+    if (n < 0) {n = -n}
+  for(i = 0; i < n.length; i++){
+    result = n[i] + result
+  }
+  return parseInt(result);
 }
